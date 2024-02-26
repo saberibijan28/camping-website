a6b0b525106bb2c55d11da8f96c693fcf8aaa044
bijan saberi <bsaberi93.com>
2024-02-26 17:02:53 +0330

    codding navbar style

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+let navBtn = document.querySelector('.nav__btn');
+let navMenu = document.querySelector(".nav-menu")
+
+
+navBtn.addEventListener('click', function(){
+    navBtn.classList.toggle('nav__btn--open')
+    navMenu.classList.toggle('nav-menu--open')
+})
