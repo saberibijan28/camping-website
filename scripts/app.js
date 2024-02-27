d134c2c56b4d870748269fab34da277d908e8519
bijan saberi <bsaberi93.com>
2024-02-27 21:24:46 +0330

    test code

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,28 @@ navBtn.addEventListener('click', function(){
     navBtn.classList.toggle('nav__btn--open')
     navMenu.classList.toggle('nav-menu--open')
 })
+
+
+let $ = document
+const players = $.querySelectorAll('.fa-play')
+const audios = $.querySelectorAll('audio')
+
+let musicName;
+
+players.forEach(function (player) {
+
+    player.addEventListener('click', function (event) {
+        musicName = event.target.dataset.name
+
+        audios.forEach(function (audio) {
+            if (audio.dataset.name === musicName) {
+                audio.currentTime = 0
+                audio.play()
+            } else {
+                audio.pause()
+            }
+        })
+
+    })
+
+})
