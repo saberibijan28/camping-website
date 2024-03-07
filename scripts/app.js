9d5de18a16c58c60534b9a4330da25fafa79dc94
bijan saberi <bsaberi93.com>
2024-03-07 12:59:06 +0330

    codding js for navbar

--- scripts/app.js
+++ scripts/app.js
@@ -8,26 +8,14 @@ navBtn.addEventListener('click', function(){
 })
 
 
-let $ = document
-const players = $.querySelectorAll('.fa-play')
-const audios = $.querySelectorAll('audio')
 
-let musicName;
+let menu = document.querySelectorAll('.menu__item')
 
-players.forEach(function (player) {
-
-    player.addEventListener('click', function (event) {
-        musicName = event.target.dataset.name
-
-        audios.forEach(function (audio) {
-            if (audio.dataset.name === musicName) {
-                audio.currentTime = 0
-                audio.play()
-            } else {
-                audio.pause()
-            }
-        })
-
+for(let i=0; i<menu.length ; i++){
+    menu[i].addEventListener('click', function(event){
+        for(let j =0; j< menu.length; j++){
+            menu[j].classList.remove('menu__link--active')
+        }
+        event.target.parentElement.classList.add('menu__link--active')
     })
-
-})
+}
